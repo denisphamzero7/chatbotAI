da79674b433412d89cea608aafe7e33c4df199aa
anhttq10 <tonthatquynhanh121020@gmail.com>
2025-10-08 02:47:21 +0700

    add guricorn conf

---
+++ gunicorn.conf.py
@@ -0,0 +1,13 @@
+bind = '0.0.0.0:8080'
+workers = 2
+threads = 2
+backlog = 512
+chdir = '/www/wwwroot/apidieuhanhnoivu.danang.gov.vn/chatbotAI'
+user = 'www'
+loglevel = 'info'
+capture_output = True
+
+errorlog = chdir + '/logs/error.log'
+accesslog = chdir + '/logs/access.log'
+access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s "%(f)s" "%(a)s"'
+pidfile = chdir + '/logs/gunicorn.pid'
